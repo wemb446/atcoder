83408fc5b245690099c3277ffb9aa95e4f5ec8b4
wemb446 <piyopiyosh1101@gmail.com>
2021-08-09 11:23:29 +0900

    abc203 b,c

--- abc203/b/main.py
+++ abc203/b/main.py
@@ -1,9 +1,5 @@
-import bisect, collections, copy, heapq, itertools, math, string
 import sys
-def I(): return int(sys.stdin.readline().rstrip())
 def LI(): return list(map(int, sys.stdin.readline().rstrip().split()))
-def S(): return sys.stdin.readline().rstrip()
-def LS(): return list(sys.stdin.readline().rstrip().split())
 
-n = I()
-a = [LI() for _ in range(n)]
+n, k = LI()
+print((n*(n+1)//2)*k*100+(k*(k+1)//2)*n)
--- abc203/c/main.py
+++ abc203/c/main.py
@@ -1,9 +1,17 @@
-import bisect, collections, copy, heapq, itertools, math, string
 import sys
-def I(): return int(sys.stdin.readline().rstrip())
 def LI(): return list(map(int, sys.stdin.readline().rstrip().split()))
-def S(): return sys.stdin.readline().rstrip()
-def LS(): return list(sys.stdin.readline().rstrip().split())
 
-n = I()
-a = [LI() for _ in range(n)]
+n, k = LI()
+friends = []
+for i in range(n):
+  a, b = LI()
+  friends.append((a, b))
+friends.sort(key=lambda x:x[0])
+
+for i in range(n):
+  p = friends[i][0]
+  if k < p:
+    break
+  k += friends[i][1]
+
+print(k)
